291c10d52eb0eacaf5f64aa781619bb10387fe50
MarkvanStratum <markvanstratum67@gmail.com>
2026-09-14 20:08:37 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -4261,7 +4261,7 @@ app.use((req, res) => {
 //	SERVER START
 //--------------------------------------------
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
 	console.log("======================================");
 	console.log("📖 HOLY CHAT SERVER RUNNING");
 	console.log(`🌍 Port: ${PORT}`);
